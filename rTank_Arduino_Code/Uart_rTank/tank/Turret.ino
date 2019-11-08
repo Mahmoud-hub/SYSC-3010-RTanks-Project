acd055cff72c6bd2921f088fd5d8d604913e6049
tomsloan1 <tsloan81@gmail.com>
2019-11-08 15:26:48 -0500

    Added more func, drive and turret control

--- rTank_Arduino_Code/Uart_rTank/tank/Turret.ino
+++ rTank_Arduino_Code/Uart_rTank/tank/Turret.ino
@@ -1,36 +1,9 @@
 void moveTurret(int x, int y) {
- 
 
-
-
-//// the previous reading from the analog input
-//int previous = 0;
-//int  Steps2Take;
-//int buttonState = 0;
-//
-//void setup() {
-//  pinMode (on, INPUT);
-//  pinMode (off, INPUT);
-//  pinMode (2,OUTPUT);
-//  Serial.begin(9600);
-//  Serial.write("start");
-//}
-//
-//  buttonState = digitalRead(on);
-// 
-// Serial.print (buttonState, DEC);
-//  Serial.println();
-//  if (buttonState == HIGH) {
-//    Serial.write("test");
-//    Steps2Take  = STEPS_PER_OUTPUT_REVOLUTION;  // Rotate CCW 1 turn
-//    turret.setSpeed(500);
-//    turret.step(Steps2Take);
-//    
-//  }
-//  else{
-//    Steps2Take  =  - STEPS_PER_OUTPUT_REVOLUTION;  // Rotate CCW 1 turn
-//    turret.setSpeed(500);
-//    turret.step(Steps2Take);
-//  }
-
+  int degress_per_step = STEPS_PER_OUTPUT_REVOLUTION / 360;
+  int new_degrees = atan(y / x);
+  int move_degrees = new_degrees - cur_degrees;
+  cur_degrees = new_degrees;
+  turret.setSpeed(500);
+  turret.step(move_degrees);
 }
