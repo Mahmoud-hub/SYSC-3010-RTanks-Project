748e389621bbe1bce3f472eae529cc6f6b46e403
MarcDonovan1 <39577801+MarcDonovan1@users.noreply.github.com>
2019-12-03 15:24:16 -0500

    Adding in code
    This portion should help the control the tank properly

---
+++ Project App/flutter_app_joypad_ble/lib/main.dart
@@ -0,0 +1,35 @@
+// Copyright 2018 The Flutter team. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_app_joypad_ble/home.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Welcome to RTanks',
+      home: MainScreen(),
+    );
+  }
+}
+
+class MainScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder(
+      stream: FirebaseAuth.instance.onAuthStateChanged,
+      builder: (context,AsyncSnapshot<FirebaseUser> snapshot) {
+        if(snapshot.connectionState == ConnectionState.waiting)
+          return FirstRoute();
+        if(!snapshot.hasData || snapshot.data == null)
+          return FirstRoute();
+        return FirstRoute();
+      },
+    );
+  }
+}
